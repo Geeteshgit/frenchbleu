c5ec3273c57b6cbca42320805992030d6b08cc58
Geetesh Dalal <geetesh472@gmail.com>
2026-06-02 06:44:52 +0530

    added faq section

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,13 @@
 import ContactHero from "@/components/contact/ContactHero";
 import ContactInfo from "@/components/contact/ContactInfo";
+import FAQSection from "@/components/contact/FAQSection";
 
 const ContactPage = () => {
   return (
     <main className="flex flex-col gap-16 pb-8">
       <ContactHero />
       <ContactInfo />
+      <FAQSection />
     </main>
   );
 };
